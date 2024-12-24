6879e94a4f4029f579ddbcc79f231103a8c1eeed
BaldvvinIV <Bananawashungry@gmail.com>
2024-12-24 21:18:15 +0300

    REACT1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Print from './Message'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="print"><Print name="John" age="20" /></div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Message.jsx
@@ -0,0 +1,8 @@
+function Print (props) {
+    return (
+        <div>
+            <h1>Hello , my name is {props.name} and my age is {props.age}</h1>
+        </div>
+    )
+}
+export default Print
